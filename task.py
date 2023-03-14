b7477eefbeb7e7940e358e08e20ea53b5638683c
ryazancev.azov <ryazancev.azov@gmail.com>
2023-03-14 23:52:54 +0300

    Задача №1 выполнена

--- task.py
+++ task.py
@@ -1,1 +1,27 @@
-# Задача №1. 
+# Задача №1. Найдите сумму цифр трехзначного числа.
+
+# *Пример:*
+# 
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) |
+# *** Рассмотрите случай числа с плавающей точкой и не обязательно 3-х значного
+
+number = int(input("Введите число: "))
+
+sum = 0
+for i in str(number):    
+    sum += int(i)
+print(sum)
+
+
+# ***
+#import re
+#number = float(input("Введите число: "))
+#
+#NewList = list(map(int, re.findall('\d', str(number))))
+#print(str(NewList))
+#
+#sum = 0
+#for i in range(len(NewList)):
+#    sum += NewList[i]    
+#print("Сумма цифр введенного числа: ", sum)
